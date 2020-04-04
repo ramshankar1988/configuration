7dfe814a43b34640e71788c800749873bf4f5627
ramshankar1988 <ramshankarsc9@gmail.com>
2020-04-04 23:13:10 +0530

    Update native.sh

--- util/install/native.sh
+++ util/install/native.sh
@@ -149,8 +149,8 @@ CONFIGURATION_VERSION=${CONFIGURATION_VERSION-$OPENEDX_RELEASE}
 ## Clone the configuration repository and run Ansible
 ##
 cd /var/tmp
-git clone https://github.com/ramshankar1988/edux.git
-cd edux
+git clone https://github.com/ramshankar1988/configuration.git
+cd configuration
 git checkout $CONFIGURATION_VERSION
 git pull
 
